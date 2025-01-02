5fce83dae010bce149b60c8e34a688c60ad79f97
Baptiste Rajaut <pokexpert30@gmail.com>
2025-01-02 14:49:14 +0100

    feat: Allow hordelib to initialize without comfy
    It could be useful for just downloading models ?

--- hordelib/initialisation.py
+++ hordelib/initialisation.py
@@ -29,6 +29,7 @@ def initialise(
     disable_smart_memory: bool = False,
     do_not_load_model_mangers: bool = True,
     models_not_to_force_load: list[str] | None = None,
+    do_not_initlalize_comfyui: bool = 
 ):
     """Initialise hordelib. This is required before using any other hordelib functions.
 
@@ -75,31 +76,33 @@ def initialise(
 
     # Modify python path to include comfyui
     set_system_path()
-
-    import hordelib.comfy_horde
-
-    hordelib.comfy_horde.do_comfy_import(
-        force_normal_vram_mode=force_normal_vram_mode,
-        extra_comfyui_args=extra_comfyui_args,
-        disable_smart_memory=disable_smart_memory,
-    )
-    if models_not_to_force_load is not None:
-        logger.debug(f"Overriding models_not_to_force_load with {models_not_to_force_load}")
-        hordelib.comfy_horde.models_not_to_force_load = models_not_to_force_load.copy()
-
-    vram_on_start_free = hordelib.comfy_horde.get_torch_free_vram_mb()
-    vram_total = hordelib.comfy_horde.get_torch_total_vram_mb()
-    vram_percent_used = round((vram_total - vram_on_start_free) / vram_total * 100, 2)
-    message_addendum = "This will almost certainly cause issues. "
-    message_addendum += "It is strongly recommended you close other applications before running the worker."
-    if vram_on_start_free < 2000:
-        logger.warning(f"You have less than 2GB of VRAM free. {message_addendum}")
-
-    if vram_percent_used > 60:
-        logger.warning(f"There was already {vram_percent_used}% of VRAM used on start. {message_addendum}")
-
-    if vram_total < 4000:
-        logger.warning("You have less than 4GB of VRAM total. It is likely that generations will happen very slowly.")
+    if not do_not_initlalize_comfyui :
+        import hordelib.comfy_horde
+    
+        hordelib.comfy_horde.do_comfy_import(
+            force_normal_vram_mode=force_normal_vram_mode,
+            extra_comfyui_args=extra_comfyui_args,
+            disable_smart_memory=disable_smart_memory,
+        )
+        if models_not_to_force_load is not None:
+            logger.debug(f"Overriding models_not_to_force_load with {models_not_to_force_load}")
+            hordelib.comfy_horde.models_not_to_force_load = models_not_to_force_load.copy()
+    
+        vram_on_start_free = hordelib.comfy_horde.get_torch_free_vram_mb()
+        vram_total = hordelib.comfy_horde.get_torch_total_vram_mb()
+        vram_percent_used = round((vram_total - vram_on_start_free) / vram_total * 100, 2)
+        message_addendum = "This will almost certainly cause issues. "
+        message_addendum += "It is strongly recommended you close other applications before running the worker."
+        if vram_on_start_free < 2000:
+            logger.warning(f"You have less than 2GB of VRAM free. {message_addendum}")
+    
+        if vram_percent_used > 60:
+            logger.warning(f"There was already {vram_percent_used}% of VRAM used on start. {message_addendum}")
+    
+        if vram_total < 4000:
+            logger.warning("You have less than 4GB of VRAM total. It is likely that generations will happen very slowly.")
+    else:
+        logger.debug(f"Hordelib initializing without ComfyUI. Hopefully this is intended (downloading models for instance")
 
     # Initialise model manager
     from hordelib.shared_model_manager import SharedModelManager
